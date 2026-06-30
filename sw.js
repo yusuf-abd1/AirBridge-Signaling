6490dcdc3760eacc735b8b8c3c33a9ed34b82894
MrY <yoseefy716.nabegh@gmail.com>
2026-06-30 18:55:24 +0300

    new

--- sw.js
+++ sw.js
@@ -1,1 +1,11 @@
-self.addEventListener('fetch', () => {}); // Allows offline load
+const CACHE_NAME = 'airbridge-v2'; // Changing this triggers an update
+
+self.addEventListener('install', (event) => {
+    self.skipWaiting(); // Force the new SW to activate immediately
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(caches.delete(CACHE_NAME)); // Delete old cache
+});
+
+self.addEventListener('fetch', () => {});
